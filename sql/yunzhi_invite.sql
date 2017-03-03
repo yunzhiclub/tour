79aa6afc05ec6f47e9b724cbe68bf5f5e5b2bcbf
tangzhenjie <2819786276@qq.com>
2017-03-03 17:15:28 +0800

    规整sql和添加首页地区城市api

--- sql/yunzhi_invite.sql
+++ sql/yunzhi_invite.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50505
+Source Server Version : 50626
 Source Host           : localhost:3306
 Source Database       : tour
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50626
 File Encoding         : 65001
 
-Date: 2017-03-02 21:23:09
+Date: 2017-03-03 16:54:05
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,23 +21,23 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `yunzhi_invite`;
 CREATE TABLE `yunzhi_invite` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `route_id` int(10) DEFAULT NULL,
-  `user_id` int(10) unsigned DEFAULT NULL,
-  `bed_id` int(10) DEFAULT NULL,
-  `start_id` int(10) DEFAULT NULL,
-  `number` int(10) DEFAULT NULL,
-  `person_num` int(10) DEFAULT NULL,
-  `pay` int(10) DEFAULT NULL,
-  `unpay` int(10) DEFAULT NULL,
-  `status` tinyint(1) DEFAULT NULL,
-  `ispublic` tinyint(1) DEFAULT NULL,
-  `ordertime` int(10) DEFAULT NULL,
+  `route_id` int(10) NOT NULL COMMENT '路线id',
+  `user_id` int(10) unsigned NOT NULL COMMENT '用户id',
+  `start_time_id` int(10) NOT NULL COMMENT '出发时间id',
+  `number` int(10) NOT NULL COMMENT '订单号',
+  `person_num` int(10) NOT NULL DEFAULT '0' COMMENT '邀约人数',
+  `pay_num` int(10) NOT NULL DEFAULT '0' COMMENT '支付人数',
+  `unpay_num` int(10) NOT NULL DEFAULT '0' COMMENT '未支付人数',
+  `status` tinyint(10) NOT NULL DEFAULT '0' COMMENT '0状态是邀约未完成1完成',
+  `is_public` tinyint(10) NOT NULL DEFAULT '1' COMMENT '0是不公开1是公开',
+  `create_time` int(40) NOT NULL DEFAULT '0' COMMENT '订单生成时间',
+  `deadline` int(40) NOT NULL DEFAULT '0' COMMENT '到期时间小与路线的结束时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of yunzhi_invite
 -- ----------------------------
-INSERT INTO `yunzhi_invite` VALUES ('1', '1', '1', '1', '1', '1', '4', '1', '1', '1', '1', '1222');
-INSERT INTO `yunzhi_invite` VALUES ('2', '1', '1', null, null, null, null, null, null, '1', null, null);
-INSERT INTO `yunzhi_invite` VALUES ('3', '2', '1', null, null, null, null, null, null, '0', null, null);
+INSERT INTO `yunzhi_invite` VALUES ('1', '1', '1', '1', '1', '4', '1', '1', '1', '1', '1222', '0');
+INSERT INTO `yunzhi_invite` VALUES ('2', '1', '1', '0', '0', '0', '0', '0', '1', '0', '0', '0');
+INSERT INTO `yunzhi_invite` VALUES ('3', '2', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0');
--- sql/yunzhi_route.sql
+++ sql/yunzhi_route.sql
@@ -2,15 +2,15 @@
 Navicat MySQL Data Transfer
 
 Source Server         : localhost
-Source Server Version : 50505
+Source Server Version : 50626
 Source Host           : localhost:3306
 Source Database       : tour
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50626
 File Encoding         : 65001
 
-Date: 2017-03-02 21:23:31
+Date: 2017-03-03 16:53:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,25 +24,26 @@ CREATE TABLE `yunzhi_route` (
   `start_city_id` int(10) DEFAULT NULL COMMENT '出发城市',
   `destination_city_id` int(10) DEFAULT NULL COMMENT '目的城市',
   `hotel_id` int(10) DEFAULT NULL COMMENT '酒店ID',
-  `beginflight_id` int(10) DEFAULT NULL COMMENT '去时航班',
-  `backflight_id` int(10) DEFAULT NULL COMMENT '返回航班',
-  `begintime` int(10) DEFAULT NULL COMMENT '开始时间',
-  `day` int(10) DEFAULT NULL COMMENT '天数',
+  `begin_flight_id` int(10) DEFAULT NULL COMMENT '去时航班id',
+  `back_flight_id` int(10) DEFAULT NULL COMMENT '返回航班id',
+  `days` int(10) DEFAULT NULL COMMENT '路线总天数',
   `description` varchar(40) DEFAULT '' COMMENT '描述信息',
-  `checkindays` int(10) DEFAULT NULL COMMENT '入住天数',
-  `checkinrooms` int(10) DEFAULT NULL COMMENT '房间数',
-  `startingprice` int(10) DEFAULT NULL COMMENT '起价',
-  `price` int(10) DEFAULT NULL COMMENT '实际价格',
-  `slidershow` int(10) DEFAULT NULL COMMENT '幻灯片',
+  `check_in_days` int(10) DEFAULT NULL COMMENT '入住天数',
+  `check_in_rooms` int(10) DEFAULT NULL COMMENT '房间数',
+  `starting_price` int(10) DEFAULT NULL COMMENT '路线起价',
+  `standard_price` int(10) DEFAULT NULL COMMENT '标价',
+  `picture_id` int(10) DEFAULT NULL COMMENT '幻灯片id',
   `deadline` int(10) DEFAULT NULL COMMENT '到期时间',
   `content` varchar(100) DEFAULT '' COMMENT '详细内容',
+  `click` int(100) DEFAULT '0' COMMENT '点击次数',
+  `service_phone` int(20) DEFAULT '0' COMMENT '客服电话',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of yunzhi_route
 -- ----------------------------
-INSERT INTO `yunzhi_route` VALUES ('1', '1', '1', '1', '1', '1', '1', '3', '这是一个路线比较简短的描述信息', '3', '3', '9688', '9000', '1', '5', '这个是一个路线的内容的详细描述');
-INSERT INTO `yunzhi_route` VALUES ('2', '1', '1', '1', '1', '1', '1', '1', '路线的描述信息2', '2', '2', '9655', '9500', '1', '5', '这是路线的详细描述2');
-INSERT INTO `yunzhi_route` VALUES ('3', '1', '2', null, null, null, null, null, '', null, null, null, null, null, null, '');
-INSERT INTO `yunzhi_route` VALUES ('4', '2', '2', null, null, null, null, null, '', null, null, null, null, null, null, '');
+INSERT INTO `yunzhi_route` VALUES ('1', '1', '1', '1', '1', '1', '3', '这是一个路线比较简短的描述信息', '3', '3', '9688', '9000', '1', '5', '这个是一个路线的内容的详细描述', '0', '0');
+INSERT INTO `yunzhi_route` VALUES ('2', '1', '1', '1', '1', '1', '1', '路线的描述信息2', '2', '2', '9655', '9500', '1', '5', '这是路线的详细描述2', '0', '0');
+INSERT INTO `yunzhi_route` VALUES ('3', '1', '2', null, null, null, null, '', null, null, null, null, null, null, '', '0', '0');
+INSERT INTO `yunzhi_route` VALUES ('4', '2', '2', null, null, null, null, '', null, null, null, null, null, null, '', '0', '0');
