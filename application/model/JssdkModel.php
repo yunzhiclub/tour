b2e4cca694905cf86d157551fe59ff25616edb40
huangshuaibin <huangshuaibin@163.com>
2017-03-04 16:14:23 +0800

    保存用户时保存图片返回URL,取出用户时URL的拼接以及返回

--- application/model/JssdkModel.php
+++ application/model/JssdkModel.php
@@ -155,7 +155,7 @@ class JssdkModel {
     $Media = new \Wechat\WechatMedia(); 
     $result = $Media->getMedia($serverId);
     if ($result === false) {
-      return fasle;
+      return false;
     }
 
     // 拼出保存图片的地址
