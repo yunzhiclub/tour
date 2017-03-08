d5a897347910bd47149af9b9764064806d0a77f0
huangshuaibin <huangshuaibin@163.com>
2017-03-08 22:04:19 +0800

    PictureController删除,上传方法

--- application/admin/controller/PictureController.php
+++ application/admin/controller/PictureController.php
@@ -28,5 +28,39 @@ class PictureController extends IndexController
     {
     	return $this->fetch();
     }
+    /**
+     * 上传图片保存
+     * @author huangshuaibin 
+     * @return [type] [description]
+     */
+    public function upload()
+    {
+        $flag = false;  //判断图片调用方法是否成功的标记
+        $file = request()->file('image');
+        $data = Request::instance()->param();
+
+        //上传图片返回false
+        if (!is_null($file)) {
+            $flag = PictureModel::saveDate($data, $file);
+        }
+        if (!$flag) {
+            return $this->error('图片上传失败', url('add'));
+        }
+
+        return $this->success('图片上传成功', url('index'));
+    }
+
+    //删除图片数据
+    public function delete()
+    {
+        $id = Request::instance()->param('id/d');
+
+        //调用软删除方法,判断是否删除成功,将is_delete赋值为1
+        if (false === PictureModel::falseDelete($id)) {
+            return $this->error('删除失败');
+        }
+
+        return $this->success('删除成功', url('index'));
+    }
 
 }
