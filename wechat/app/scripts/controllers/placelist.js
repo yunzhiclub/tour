6aa680642a1ce7c3d40214a2b0a9ad92c79656be
tangzhenjie <2819786276@qq.com>
2017-03-09 18:57:45 +0800

    加上m层工具server和加上城市列表cv

--- wechat/app/scripts/controllers/placelist.js
+++ wechat/app/scripts/controllers/placelist.js
@@ -8,13 +8,14 @@
  * Controller of the wechatApp
  */
 angular.module('wechatApp')
-    .controller('PlacelistCtrl', ['$scope', '$stateParams', 'destination', function($scope, $stateParams, destination) {
+    .controller('PlacelistCtrl', ['$scope', '$stateParams', 'destination', 'commonTools', function($scope, $stateParams, destination, commonTools) {
        
         $scope.countrys = [];
         if ($stateParams.regionId !== undefined) {
         	destination.getCountrysByRegionId($stateParams.regionId).then(function successCallBack(response) {
-        		console.log(response);
-        		$scope.countrys = response;
+        		
+        		$scope.countrys = commonTools.formatArray(response);
+                
         	}, function errorCallBack(response) {
 
         	});
