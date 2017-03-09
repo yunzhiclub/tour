234a0af8fff45065e5f88a40f80ca6506b7dfff8
mskies <3792535@qq.com>
2017-03-09 15:51:18 +0800

    将数据过滤写入C层。TODO：转移到M层。

--- wechat/app/scripts/controllers/placelist.js
+++ wechat/app/scripts/controllers/placelist.js
@@ -9,14 +9,31 @@
  */
 angular.module('wechatApp')
     .controller('PlacelistCtrl', ['$scope', '$stateParams', 'destination', function($scope, $stateParams, destination) {
-       
+        var format = function(items, length = 3) {
+            var data = [];
+            var datas = [];
+            angular.forEach(items, function(value, key) {
+                data.push(value);
+                if (0 === ((key + 1) % length)) {
+                    datas.push(data);
+                    data = [];
+                }
+            });
+
+            if (items.length % length !== 0) {
+                datas.push(data);
+            }
+            return datas;
+        };
+
         $scope.countrys = [];
         if ($stateParams.regionId !== undefined) {
         	destination.getCountrysByRegionId($stateParams.regionId).then(function successCallBack(response) {
-        		console.log(response);
-        		$scope.countrys = response;
+        		$scope.countrys = format(response);
         	}, function errorCallBack(response) {
 
         	});
-        }
+        };
+
+
     }]);
