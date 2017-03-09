3e5ff43b0ecd6e3844efc390ae7ad2d5a0be53c1
chuhang123 <1093609364@qq.com>
2017-03-09 23:40:11 +0800

    出发城市增删改查

--- application/model/StartCityModel.php
+++ application/model/StartCityModel.php
@@ -7,5 +7,37 @@ namespace app\model;
  */
 class StartCityModel extends ModelModel
 {
+	/**
+	 * 判断该出发城市是否为某航班的出发城市
+	 * @return boolean 
+	 * @author chuhang 
+	 */
+	public function isFlightStartCity() {
+		$FlightModels = FlightModel::all();
 
+		foreach ($FlightModels as $FlightModel) {
+			if ($FlightModel->up_city_id === $this->id) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * 判断该出发城市是否为某路线出发城市
+	 * @return boolean 
+	 * @author chuhang 
+	 */
+	public function isRouteStartCity() {
+		$RouteModels = RouteModel::all();
+
+		foreach ($RouteModels as $RouteModel) {
+			if ($RouteModel->start_city_id === $this->id) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
