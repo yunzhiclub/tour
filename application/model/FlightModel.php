22b8944593f69e97cdcce2f7cc5f7723be6bee1e
chuhang123 <1093609364@qq.com>
2017-03-09 22:05:29 +0800

    去除字段

--- application/model/FlightModel.php
+++ application/model/FlightModel.php
@@ -37,45 +37,6 @@ class FlightModel extends ModelModel
 
 		return $this->DestinationCityModel;
 	}
-	/**
-	 * 获取出发城市名称通过id
-	 * @param  int $id 城市id
-	 * @return String    城市名称
-	 * @author chuhang 
-	 */
-	static public function getStartCityNameById($id) {
-
-		$StartCityModel = StartCityModel::get($id);
-
-		//判断出发城市是否为空
-		if (null !== $StartCityModel) {
-			$StartCityName = $StartCityModel->getData('name');
-		} else {
-			$StartCityName = '无';
-		}
-
-		return $StartCityName;
-	}
-
-	/**
-	 * 获取目的地城市名称通过id
-	 * @param  int $id 目的地城市id
-	 * @return String     目的地城市姓名
-	 * @author chuhang 
-	 */
-	static public function getDestinationCityNameById($id) {
-
-		$DestinationCityModel = DestinationCityModel::get($id);
-
-		//判断目的地是否为空
-		if (null !== $DestinationCityModel) {
-			$DestinationCityName = $DestinationCityModel->getData('name');
-		} else {
-			$DestinationCityName = '无';
-		}
-		
-		return $DestinationCityName;
-	}
 
 	/**
 	 * 对money过滤，因为具有strfmon 的系统才有 money_format() 函数。 例如 Windows 不具备，所以 Windows 系统上 money_format() 未定义。用引此方法对money过滤
@@ -106,22 +67,5 @@ class FlightModel extends ModelModel
 	static public function substrTime($time) {
 		$result = substr($time, 0, -3);
 		return $result;
-	}
-
-	/**
-	 * 获取飞机舱型，0，头等舱，1，公务舱，2，经济舱
-	 * @param  int $val 舱型对应的数字
-	 * @return String      舱型名称
-	 * @author chuhang 
-	 */
-	static public function getTypeName($val) {
-
-		if ($val === 0) {
-			return '头等舱';
-		} elseif ($val === 1) {
-			return '公务舱';
-		}
-
-		return '经济舱';
 	}
 }
