abfd4c3369f3442cce9e0b2b273f6554813de81d
huangshuaibin <huangshuaibin@163.com>
2017-03-13 20:56:09 +0800

    EvaluateModel中获取CustomerModel对象的方法

--- application/model/EvaluateModel.php
+++ application/model/EvaluateModel.php
@@ -6,6 +6,17 @@ namespace app\model;
  */
 class EvaluateModel extends ModelModel
 {
+	private $CustomerModel = null;
+	private $RouteModel = null;
+
+	public function getCustomerModel()
+	{
+		if (null === $this->CustomerModel) {
+			$CustomerId = $this->getData('customer_id');
+			$this->CustomerModel = CustomerModel::get($CustomerId);
+		}
+		return $this->CustomerModel;
+	}
 	/**
 	 **通过路线id查询对应的评价		
 	 * @param  int $RouteId 路线id
