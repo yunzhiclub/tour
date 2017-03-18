a38603111c0c3757cfe5c31b8ef4ba7fbe1e016f
huangshuaibin <huangshuaibin@163.com>
2017-03-18 21:32:30 +0800

    邀约管理，index方法，删除delete方法

--- application/admin/controller/InviteController.php
+++ application/admin/controller/InviteController.php
@@ -1,11 +1,16 @@
 <?php
 namespace app\admin\controller;
 use think\Controller;
+use app\model\InviteModel;
+use think\Request;
 
 class InviteController extends IndexController
 {
 	public function index()
 	{
+		$InviteModel = new InviteModel;
+		$InviteModels = $InviteModel->paginate(10);
+		$this->assign('InviteModels', $InviteModels);
 		return $this->fetch();
 	}
 	public function add()
@@ -15,6 +20,26 @@ class InviteController extends IndexController
 	public function edit()
 	{
 		return $this->fetch();
+	}
+	public function delete()
+	{
+		$Id = Request::instance()->param('id/d');
+
+		if (is_null($Id) || 0 === $Id) {
+			return $this->error('未获取ID信息');
+		}
+
+		$InviteModel = InviteModel::get($Id);
+
+		if (null === $InviteModel) {
+			return $this->error('不存在id为' . $Id . '的数据');
+		}
+
+		if (!$InviteModel->delete()) {
+			return $this->error('删除失败:' . $InviteModel->getError());
+		}
+
+		return $this->success('删除成功', url('index'));
 	}
 
 	public function bedDetail()
