a78c21c754dd7188d27e38f65966ae3868f088c5
chuhang123 <1093609364@qq.com>
2017-03-18 15:44:04 +0800

    ngflow尝试

--- application/admin/controller/PictureController.php
+++ application/admin/controller/PictureController.php
@@ -2,15 +2,30 @@
 namespace app\admin\controller;
 use think\Controller;
 use think\Request;
+use app\model\PictureModel;
+use think\Config;
 /**
 * 图片管理
 */
-class UploadController extends IndexController
+class PictureController extends IndexController
 {
     public function index()
-    {
+    {	
+    	Config::set('app_trace', false);
+    	//获取上传文件名
         $data = Request::instance()->param();
         var_dump($data);
-        var_dump(1);
+
+
+        // $info = $file->move(ROOT_PATH . 'public' . DS . 'upload');
+        // $Company->data($data);
+        // $Company->logo_url = '__PUBLIC__/upload/' . date('Ymd') . '/' . $info->getFilename();
+    }
+
+    public function upload()
+    {
+    	//获取上传文件名
+        $data = Request::instance()->param();
+        var_dump($data);
     }
 }
