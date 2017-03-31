d5545bfda76b25fbebd8de133c3c19bbc2d79a74
tangzhenjie <2819786276@qq.com>
2017-03-31 14:27:46 +0800

    添加部分indetail的v层动态数据

--- wechat/app/scripts/controllers/ivdetail.js
+++ wechat/app/scripts/controllers/ivdetail.js
@@ -8,9 +8,19 @@
  * Controller of the wechatApp
  */
 angular.module('wechatApp')
-  .controller('IvdetailCtrl', ['$scope', 'invitation', '$stateParams', function ($scope, invitation, $stateParams){
+  .controller('IvdetailCtrl', ['$scope', 'invitation', '$stateParams', 'commonTools', 'config', function ($scope, invitation, $stateParams, commonTools, config){
   	// 应邀的对象实体
    	var index = $stateParams.invitationIndex;
    	var invitations = invitation.getInvitations();
    	var invitation = invitations[index];
+   	
+   	// 为获取来的图片URL加上前缀
+       $scope.urlPrefix = config.siteUrl + 'public/upload/';
+
+   	// 求人均的价格
+   	$scope.perMoney = Math.ceil(invitation.totalMoney/6);
+   	invitation.beds = commonTools.formatArray(invitation.beds, 2);
+   	console.log(invitation);
+   	$scope.invitation = invitation;
+
   }]);
