082aca47ca7b7f6f1dfbe07858bda9e0422bbf5e
litian1218 <1181551049@qq.com>
2017-03-31 20:35:40 +0800

    订单管理模糊查询

--- application/admin/controller/OrderController.php
+++ application/admin/controller/OrderController.php
@@ -2,6 +2,8 @@
 namespace app\admin\controller;
 use think\Controller;
 use app\model\OrderModel;
+use think\Request;
+
 /**
 * 订单管理
 */
@@ -11,7 +13,10 @@ class OrderController extends IndexController
     public function index()
     {
         $OrderModel = new OrderModel;
-        $OrderModels = $OrderModel->paginate();
+
+        // 模糊查询
+        $number = Request::instance()->get('number');
+        $OrderModels = $OrderModel->where('number', 'like', '%' . $number . '%')->paginate();
         $this->assign('OrderModels', $OrderModels);
         return $this->fetch();
     }
