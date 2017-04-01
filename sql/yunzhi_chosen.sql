263b65f59642b87f899dd402f18797acc2c29464
chuhang123 <1093609364@qq.com>
2017-04-01 23:14:00 +0800

    上传sql文件

--- sql/yunzhi_chosen.sql
+++ sql/yunzhi_chosen.sql
@@ -1,32 +1,42 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50505
-Source Host           : localhost:3306
+Source Server         : test
+Source Server Version : 50625
+Source Host           : 127.0.0.1:3306
 Source Database       : tour
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50625
 File Encoding         : 65001
 
-Date: 2017-03-02 21:21:51
+Date: 2017-04-01 23:13:10
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `yunzhi_chosen`
+-- Table structure for yunzhi_chosen
 -- ----------------------------
 DROP TABLE IF EXISTS `yunzhi_chosen`;
 CREATE TABLE `yunzhi_chosen` (
-  `id` int(10) NOT NULL AUTO_INCREMENT COMMENT '精选编号',
-  `route_id` int(10) DEFAULT NULL COMMENT '路线id',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '精选编号',
+  `route_id` int(10) unsigned NOT NULL COMMENT '路线id',
+  `weight` int(11) NOT NULL,
+  `update_time` int(11) unsigned NOT NULL,
+  `create_time` int(11) unsigned NOT NULL,
+  `is_delete` tinyint(1) unsigned zerofill NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=55 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of yunzhi_chosen
 -- ----------------------------
-INSERT INTO `yunzhi_chosen` VALUES ('1', '1');
-INSERT INTO `yunzhi_chosen` VALUES ('2', '2');
+INSERT INTO `yunzhi_chosen` VALUES ('47', '77', '343', '1491057766', '1491057701', '1');
+INSERT INTO `yunzhi_chosen` VALUES ('48', '80', '4', '1491058364', '1491058230', '1');
+INSERT INTO `yunzhi_chosen` VALUES ('49', '80', '4', '1491058408', '1491058364', '1');
+INSERT INTO `yunzhi_chosen` VALUES ('50', '80', '4', '1491058635', '1491058408', '1');
+INSERT INTO `yunzhi_chosen` VALUES ('51', '80', '4', '1491058887', '1491058635', '1');
+INSERT INTO `yunzhi_chosen` VALUES ('52', '80', '4', '1491059086', '1491058887', '1');
+INSERT INTO `yunzhi_chosen` VALUES ('53', '80', '4', '1491059113', '1491059086', '1');
+INSERT INTO `yunzhi_chosen` VALUES ('54', '80', '4', '1491059445', '1491059113', '1');
