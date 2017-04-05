ddb24254732a046e39e4c549915ff1e024c99a01
chuhang123 <1093609364@qq.com>
2017-04-05 22:59:32 +0800

    修改sql文件

--- sql/yunzhi_evaluate.sql
+++ sql/yunzhi_evaluate.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50625
 File Encoding         : 65001
 
-Date: 2017-03-16 16:14:38
+Date: 2017-04-05 22:59:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,19 +20,19 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `yunzhi_evaluate`;
 CREATE TABLE `yunzhi_evaluate` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `customer_id` int(11) DEFAULT NULL,
-  `route_id` int(10) DEFAULT NULL,
-  `star` int(10) DEFAULT NULL,
-  `content` varchar(200) DEFAULT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) unsigned NOT NULL,
+  `route_id` int(10) unsigned NOT NULL,
+  `star` int(10) NOT NULL,
+  `content` text,
   `is_delete` tinyint(1) unsigned zerofill NOT NULL,
-  `update_time` int(11) unsigned zerofill NOT NULL,
-  `create_time` int(11) unsigned zerofill NOT NULL,
+  `update_time` int(11) unsigned NOT NULL,
+  `create_time` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of yunzhi_evaluate
 -- ----------------------------
-INSERT INTO `yunzhi_evaluate` VALUES ('1', '1', '1', '5', '这是评价1', '0', '01489501499', '00000000000');
-INSERT INTO `yunzhi_evaluate` VALUES ('2', '2', '1', '4', '这是评价2', '0', '01489501493', '00000000000');
+INSERT INTO `yunzhi_evaluate` VALUES ('1', '1', '1', '5', '这是评价1', '0', '0', '0');
+INSERT INTO `yunzhi_evaluate` VALUES ('2', '1', '2', '4', '这是评价2', '0', '0', '0');
