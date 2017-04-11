05de293b88ca2f275f7a7fad78ac383d39d5fa18
tangzhenjie <2819786276@qq.com>
2017-04-11 20:59:18 +0800

    发布邀约时获取全部地区和地区全部城市的页面添加获取不到数据的提示页面

--- wechat/app/scripts/controllers/choseroute.js
+++ wechat/app/scripts/controllers/choseroute.js
@@ -9,11 +9,27 @@
  */
 angular.module('wechatApp')
     .controller('ChoserouteCtrl', ['$scope', 'order', 'destination', 'commonTools',  function($scope, order, destination, commonTools) {
+        
+        // 数据加载过程中隐藏邀约页面
+        $scope.loading = true;
+
+        // 为找到数据时显示数据未找到界面
+        $scope.isEmpty = true;
+
         $scope.regions = [];
         destination.getAllRegions().then(function successCallBack(response) {
         	console.log(response);
         	// 变为二维数组
             $scope.regions = commonTools.formatArray(response, 4);
+            // 判断是否获取邀约信息,用来判断是否显示未获取到数据的页面
+            if (response.length > 0) {
+                $scope.isEmpty = false;
+            } else {
+                $scope.isEmpty = true;
+            }
+
+            // 数据加载完成
+            $scope.loading = false;
         }, function errorCallBack(response) {
         	console.log(response);
         });
