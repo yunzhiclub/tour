51a59de019b7b3daf33ae07bf81e746505a57abb
tangzhenjie <2819786276@qq.com>
2017-04-19 17:38:38 +0800

    修改冲突的名字

--- wechat/app/scripts/controllers/ivdetail.js
+++ wechat/app/scripts/controllers/ivdetail.js
@@ -14,7 +14,7 @@ angular.module('wechatApp')
         var index = $stateParams.invitationIndex;
         $scope.index = index;
         var invitations = invitation.getInvitations();
-        var invitation = invitations[index];
+        var invite = invitations[index];
         var getAge = function(birthday) {
             if (birthday !== null) {
                 // 变为字符串属性
@@ -49,7 +49,7 @@ angular.module('wechatApp')
         }
         console.log($scope.customer);
         // 判断该邀约是否有资格选择 有资格isQualified 为1 否则为 0
-        angular.forEach(invitation.beds, function(bed) {
+        angular.forEach(invite.beds, function(bed) {
         	// 给oldType赋默认值
         	var oldType = -1;
 
@@ -66,12 +66,12 @@ angular.module('wechatApp')
                 bed.isQualified = 1; // 为了测试先都允许选择房间
             }
         });
-        // 为获取来的图片URL加上前缀
-        $scope.urlPrefix = config.siteUrl + 'public/upload/';
 
         // 求人均的价格
-        $scope.perMoney = Math.ceil(invitation.totalMoney / 6);
-        invitation.beds = commonTools.formatArray(invitation.beds, 2);
-        console.log(invitation);
+        $scope.perMoney = Math.ceil(invite.totalMoney / 6);
+        invite.beds = commonTools.formatArray(invite.beds, 2);
+
+        // 向临时的factory中保存已经修改好的邀约
+
         $scope.invitation = invitation;
     }]);
