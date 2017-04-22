506abb6c012b16f6161733409fc1ccca502bff81
zdreamcn <55185294@qq.com>
2017-04-23 00:50:17 +0800

    完成首页推荐模块
    完成首页路线、首页城市、首页地区

---
+++ application/admin/controller/HomeRegionController.php
@@ -0,0 +1,80 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use think\Request;
+use app\model\HomeRegionModel;
+use app\model\RegionModel;
+
+class  HomeRegionController extends IndexController
+{
+	public function index()
+	{
+		//获取信息
+		$data = input('get.');
+		//模糊查询
+		$HomeRegionModel = new HomeRegionModel;
+		//为查询下拉框显示全部信息
+		$map['is_delete'] = 0;
+        $HomeRegionModelAll = $HomeRegionModel ->where($map)->select();
+        //设置多条件查询
+		$HomeRegionModels = $HomeRegionModel->getSearchInfo($data)->paginate();
+		$this->assign('HomeRegionModels', $HomeRegionModels);
+		$this->assign('HomeRegionModelAll', $HomeRegionModelAll);
+ 		return $this->fetch();
+	}
+	public function edit()
+	{   //获取要编辑的ID
+		$id = Request::instance()->param('id');
+		//向editV层传递区域表信息（显示所有未删除的区域名称）
+		$RegionModel = new RegionModel;
+		$map['is_delete'] = 0;
+		$RegionModels = $RegionModel->where($map)->select();
+		$HomeRegionModel = HomeRegionModel::get($id);
+		$this->assign('HomeRegionModel', $HomeRegionModel);
+		$this->assign('RegionModels', $RegionModels);
+
+		return $this->fetch();
+	}
+	public function add()
+	{
+		//获取区域信息
+		$RegionModel = new RegionModel;
+		$map['is_delete'] = 0;
+
+		$RegionModels = $RegionModel->where($map)->select();
+
+		$this->assign('RegionModels', $RegionModels);
+		
+		return $this->fetch();
+	}
+	public function delete()
+	{
+		$id = Request::instance()->param('id');
+		$HomeRegionModel = HomeRegionModel::get($id);
+		$HomeRegionModel->is_delete = 1;
+		if( false === $HomeRegionModel->save()){
+			return $this->error($HomeRegionModel->getError());
+		}
+		return $this->success('删除成功');
+	}
+	public function save()
+	{
+		$data = Request::instance()->param();
+		$HomeRegionModel = new HomeRegionModel;
+		if(false === $HomeRegionModel->save($data)){
+			return $this->error($HomeRegionModel->getData());
+		}
+		return $this->success('添加成功', url('index'));
+	}
+	public function update()
+	{
+		$data = Request::instance()->param();
+		$HomeRegionModel = HomeRegionModel::get($data['id']);
+		if(false === $HomeRegionModel->isUpdate()->save($data)){
+			return $this->error($HomeRegionModel->getError());
+		}
+
+		return $this->success('操作成功', url('index'));
+	}
+
+}
--- application/model/HomeRecommendModel.php
+++ application/model/HomeRecommendModel.php
@@ -41,4 +41,50 @@ class HomeRecommendModel extends ModelModel
 
 		return $temp;
 	}
+
+	/**
+	 * 根据用户的筛选条件查询信息
+	 * @param  array $data [用户的查询条件]
+	 * @return [object]       [首页推荐信息]
+	 * @author zhangmengxiang
+	 */
+	public function getSearchInfo($data) {
+        $map['is_delete'] = 0;
+
+		if (isset($data['expiration_time']) && $data['expiration_time'] !== '0') {
+			$map['expiration_time'] = $data['expiration_time'];
+		}
+		if (isset($data['weight']) && $data['weight'] !== '0') {
+			$map['weight'] = $data['weight'];
+		}
+		
+		
+		$this->where($map);
+		
+		unset($map);
+		return $this;
+	}
+	/**
+	 * 获取首页推荐的路线、首页推荐基本信息	
+	 * @return [array] 
+	 * @author zhangmengxiang
+	 */
+
+	static public function getBasicInfo()
+	{
+		$map = [];
+		$map['is_delete'] = 0;
+		
+		//获取路线信息
+		$RouteModel = new RouteModel;
+		$result['RouteModels'] = $RouteModel->where($map)->select();
+		//获取首页推荐信息
+		$HomeRecommendModel = new HomeRecommendModel;
+		$result['HomeRecommendModels'] = $HomeRecommendModel->where($map)->select();
+
+		unset($HomeRecommendModel);
+		unset($RouteModel);
+
+		return $result;
+	}
 }
