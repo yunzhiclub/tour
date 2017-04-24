8c960d4d464672bb919efdbbdd750b7933c7db86
chuhang123 <1093609364@qq.com>
2017-04-24 21:04:44 +0800

    将deadline改为end_time,增加deadline字段

--- sql/yunzhi_route.sql
+++ sql/yunzhi_route.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50625
 File Encoding         : 65001
 
-Date: 2017-04-21 16:11:12
+Date: 2017-04-24 21:03:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,7 +34,7 @@ CREATE TABLE `yunzhi_route` (
   `starting_price` int(10) NOT NULL COMMENT '路线起价',
   `actual_price` int(10) unsigned NOT NULL,
   `standard_price` int(10) NOT NULL COMMENT '标价',
-  `deadline` int(11) unsigned NOT NULL COMMENT '出发日期——止',
+  `end_time` int(11) unsigned NOT NULL COMMENT '出发日期——止',
   `content` text NOT NULL COMMENT '详细内容',
   `click` int(11) DEFAULT '0' COMMENT '点击次数',
   `service_phone` varchar(40) DEFAULT '' COMMENT '客服电话',
@@ -43,11 +43,13 @@ CREATE TABLE `yunzhi_route` (
   `update_time` int(11) unsigned NOT NULL,
   `start_time` int(11) unsigned NOT NULL COMMENT '开始日期',
   `begin_time` int(11) unsigned NOT NULL COMMENT '出发日期——起',
+  `deadline` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=86 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of yunzhi_route
 -- ----------------------------
-INSERT INTO `yunzhi_route` VALUES ('1', '法国+瑞士+意大利+德国11日9晚跟团游', '1', '1', '1', '28', '28', '11', '赠送荷兰+库肯霍夫公园+双宫入内+滴滴湖', '10', '4', '7799', '7799', '8000', '20170406', '                                                                                                                                                                                                                                                                                                                                                                                                            <p></p><ul><li>★ 【优质保证】：全程往返优质航空，南进北出，不走回头路</li><li>★ 【深度游览】：罗马，威尼斯，佛罗伦萨的宁静悠远，精心设计</li><li>★ 【精心赠送】：令人魂牵梦萦的库肯霍夫公园，赏荷兰风光</li></ul>', '0', '022-232332', '0', '1491828544', '1491828544', '20160401', '20170402');
-INSERT INTO `yunzhi_route` VALUES ('82', '法国+意大利+瑞士10日8晚跟团游', '1', '1', '1', '28', '28', '10', '一价全含+勃朗峰+金色山口车+双宫+双游船', '9', '3', '12699', '12699', '18000', '20170415', '                                                                                                                                                                                    <p></p><ul><li>★ 【春季抢购】全程四人WIFI+可异地按指纹+出签率高</li><li>★ 【行程升级】全程三星-四星酒店+勃朗峰+双宫入内讲解+塞纳河游船</li><li>★ 【法国深度】：威尼斯本岛+威尼斯彩虹岛布尔诺 +黄金大运河</li></ul>                                                                                                                                                <p><br></p>', '0', '022-3242342', '0', '1491827868', '1491827868', '20170408', '20170408');
+INSERT INTO `yunzhi_route` VALUES ('1', '法国+瑞士+意大利+德国11日9晚跟团游', '1', '1', '1', '28', '28', '11', '赠送荷兰+库肯霍夫公园+双宫入内+滴滴湖', '10', '4', '7799', '7799', '8000', '1492963200', '                                                                                                                                                                                                                                                                                                                                                                                                                                                <p></p><ul><li>★ 【优质保证】：全程往返优质航空，南进北出，不走回头路</li><li>★ 【深度游览】：罗马，威尼斯，佛罗伦萨的宁静悠远，精心设计</li><li>★ 【精心赠送】：令人魂牵梦萦的库肯霍夫公园，赏荷兰风光</li></ul>                                    ', '0', '022-232332', '0', '1492779823', '1492779823', '1492704000', '1492704000', '1492779728');
+INSERT INTO `yunzhi_route` VALUES ('82', '法国+意大利+瑞士10日8晚跟团游', '1', '1', '1', '28', '28', '10', '一价全含+勃朗峰+金色山口车+双宫+双游船', '9', '3', '12699', '12699', '18000', '1492963200', '                                                                                                                                                                                                                        <p></p><ul><li>★ 【春季抢购】全程四人WIFI+可异地按指纹+出签率高</li><li>★ 【行程升级】全程三星-四星酒店+勃朗峰+双宫入内讲解+塞纳河游船</li><li>★ 【法国深度】：威尼斯本岛+威尼斯彩虹岛布尔诺 +黄金大运河</li></ul>                                                                                                                                                <p><br></p>                                    ', '0', '022-3242342', '0', '1492779728', '1492779728', '1492704000', '1492704000', '1492779728');
+INSERT INTO `yunzhi_route` VALUES ('84', '1', '1', '1', '1', '28', '28', '1', '1223', '11', '1', '1', '1', '1', '1493049600', '                                                                        <p>11</p>                                    ', '0', '1', '0', '1493038955', '1493038955', '1493913600', '1492963200', '1492876800');
