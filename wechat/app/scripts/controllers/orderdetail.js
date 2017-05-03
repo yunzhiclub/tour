b3914a8450d4de4ccba68de8672685d9235d8886
tangzhenjie <2819786276@qq.com>
2017-05-03 22:19:43 +0800

    分享链接完成

--- wechat/app/scripts/controllers/orderdetail.js
+++ wechat/app/scripts/controllers/orderdetail.js
@@ -8,13 +8,18 @@
  * Controller of the wechatApp
  */
 angular.module('wechatApp')
-  .controller('OrderdetailCtrl', ['$scope', 'customer', '$stateParams', 'commonTools', function ($scope, customer, $stateParams, commonTools) {
+  .controller('OrderdetailCtrl', ['$scope', 'customer', '$stateParams', 'commonTools', 'jssdk', '$location', function ($scope, customer, $stateParams, commonTools, jssdk, $location) {
       var orderId = $stateParams.orderId;
       var openId = $scope.customer.openid;
       customer.getOrderDetailById(orderId,openId).then(function successCallBack (response) {
-          $scope.beds = commonTools.formatArray(response, 2);
-          console.log($scope.beds);
+          $scope.beds = commonTools.formatArray(response.beds, 2);
+          $scope.inviteId = response.inviteId;
       },function errorCallBack() {
 
-      })
+      });
+      $scope.shareLinkToFriend = function () {
+          // 生成的链接
+          var link = $location.protocol() + '://' + $location.host() + ':' +  $location.port() + '/#!/ivdetail/' + orderId;
+          jssdk.shareLinkToFriend(link);
+      }
   }]);
