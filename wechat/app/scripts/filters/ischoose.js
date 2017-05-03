1c45f9149ad118abd6524cc215c932a4404f9159
tangzhenjie <2819786276@qq.com>
2017-05-03 21:13:34 +0800

    完成查看订单详情界面

---
+++ wechat/app/scripts/filters/ischoose.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name wechatApp.filter:ischoose
+ * @function
+ * @description
+ * # ischoose
+ * Filter in the wechatApp.
+ */
+angular.module('wechatApp')
+  .filter('ischoose', function () {
+    return function (status) {
+        var result = "已有应邀人";
+        if(status === undefined){
+            result = "可应邀";
+        }
+
+        return result;
+    };
+  });
